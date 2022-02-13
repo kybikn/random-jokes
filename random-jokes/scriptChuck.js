6f99327c287616a730108dd8eef612eb59d97811
Kybik <82117054+kybikn@users.noreply.github.com>
2022-02-14 02:57:48 +0300

    feat: add animation and js

--- random-jokes/scriptChuck.js
+++ random-jokes/scriptChuck.js
@@ -1,3 +1,20 @@
-const url = "https://api.icndb.com/jokes";
-const data = url;
-data[0].text;
+const urlEng = "https://api.icndb.com/jokes/random?escape=javascript";
+const jokeElement = document.querySelector(".joke");
+const btn = document.querySelector(".btn");
+
+async function getJoke() {
+  const res = await fetch(urlEng); // ждет ответа от сервера по запросу (fetch)
+  const data = await res.json(); // убирает кавычки с ключей
+  //   const data = JSON.parse(res);
+  // const dataToSend = JSON.stringify(data); //добавляет кавычки к ключам для отправки в запросе, противоположно JSON.parse и .json()
+
+  return data["value"]["joke"];
+}
+
+async function setJoke() {
+  let joke = await getJoke();
+  jokeElement.textContent = joke;
+}
+
+window.addEventListener("load", setJoke);
+btn.addEventListener("click", setJoke);
