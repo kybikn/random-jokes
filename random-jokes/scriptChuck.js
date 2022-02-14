7b1d37eefc868229599613fe1657c71e3090b2fe
Kybik <82117054+kybikn@users.noreply.github.com>
2022-02-14 04:07:49 +0300

    feat: add animation

--- random-jokes/scriptChuck.js
+++ random-jokes/scriptChuck.js
@@ -1,37 +1,3 @@
-import i18Obj from "./translate.js";
-
-const switchlngBox = document.querySelector(".switch-lng");
-const translateElems = document.querySelectorAll("[data-i18]");
-const langButtons = document.querySelectorAll(".switchlng");
-
-switchlngBox.addEventListener("click", setLanguage);
-
-function getTranslate(event) {
-  if (!event.target.dataset.language) {
-    return;
-  }
-  const language = event.target.dataset.language;
-  const translation = i18Obj[language];
-
-  translateElems.forEach((element) => {
-    // ключ data-i18
-    const translationKey = element.dataset.i18;
-    // проверка
-    if (translation[translationKey]) {
-      if (element.placeholder) {
-        element.placeholder = translation[translationKey];
-      } else {
-        element.textContent = translation[translationKey];
-      }
-    }
-  });
-
-  langButtons.forEach((element) => element.classList.remove("active"));
-  event.target.classList.add("active");
-}
-
-// старое
-
 const urlEng = "https://api.icndb.com/jokes/random?escape=javascript";
 const jokeElement = document.querySelector(".joke");
 const btn = document.querySelector(".btn");
